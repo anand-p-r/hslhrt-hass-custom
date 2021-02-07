3e2032536ad45f03f4a81855803a7b89161b82ea
Anand Radhakrishnan <keethyanandpr@gmail.com>
2021-02-07 22:28:47 +0200

    added destination filter for a stop

--- custom_components/config_flow.py
+++ custom_components/config_flow.py
@@ -19,7 +19,9 @@ from .const import (
     ERROR,
     ALL,
     VAR_NAME_CODE,
-    ROUTE
+    ROUTE,
+    DESTINATION,
+    ROUTE_DEST
 )
 
 
@@ -30,12 +32,14 @@ async def validate_user_config(hass: core.HomeAssistant, data):
     """
     name_code = data[NAME_CODE]
     route = data[ROUTE]
+    dest = data[DESTINATION]
 
     errors = ""
     stop_code = None
     stop_name = None
     stop_gtfs = None
     ret_route = None
+    ret_dest = None
 
     # Check if there is a valid stop for the given name/code
     try:
@@ -51,7 +55,8 @@ async def validate_user_config(hass: core.HomeAssistant, data):
             STOP_CODE: None, 
             STOP_NAME: None, 
             STOP_GTFS: None, 
-            ROUTE: None, 
+            ROUTE: None,
+            DESTINATION: None,
             ERROR: errors
             }
 
@@ -66,21 +71,40 @@ async def validate_user_config(hass: core.HomeAssistant, data):
                 stop_name = stop_data.get("name", "")
                 stop_code = stop_data.get("code", "")
                 if stop_name != "" and stop_gtfs != "":
-                    stop = stop_name
-                    gtfs = stop_gtfs
-                    if route.lower() != ALL:
+                    ## Specific route should be checked
+                    ## Ignore destination filter
+                    if (route.lower() != ALL) or (dest.lower() != ALL):
                         routes = stop_data.get("routes", None)
                         if routes is not None:
                             for rt in routes:
-                                rt_name = rt.get("shortName", "")
-                                if rt_name != "":
-                                    if rt_name.lower() == route.lower():
-                                        ret_route = route    
+                                if route.lower() != ALL:
+                                    rt_name = rt.get("shortName", "")
+                                    if rt_name != "":
+                                        if rt_name.lower() == route.lower():
+                                            ret_route = route
+                                            break
+                                else:
+                                    patterns = rt.get("patterns", None)
+                                    if patterns is not None:
+                                        break_loop = False
+                                        for pattern in patterns:
+                                            head_sign = pattern.get("headsign", "")
+                                            if head_sign != "":
+                                                if dest.lower() in head_sign.lower():
+                                                    ret_dest = head_sign
+                                                    break_loop = True
+                                                    break 
+
+                                        if break_loop:
+                                            break                              
                     else:
                         ret_route = route
 
-                    if ret_route is None:
+                    if (route.lower() != ALL) and (ret_route == None):
                         errors = "invalid_route"
+                    else:
+                        if (dest.lower() != ALL) and (ret_dest == None):
+                            errors = "invalid_destination"
                 else:
                     _LOGGER.error("Name or gtfs is blank")
                     errors = "invalid_name_code"
@@ -99,8 +123,9 @@ async def validate_user_config(hass: core.HomeAssistant, data):
         STOP_NAME: stop_name,
         STOP_GTFS: stop_gtfs,
         ROUTE: ret_route,
+        DESTINATION: ret_dest,
         ERROR: errors
-        }
+    }
 
 
 class HSLHRTConfigFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
@@ -118,26 +143,30 @@ class HSLHRTConfigFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         if user_input is not None:
 
             valid = await validate_user_config(self.hass, user_input)
-
             await self.async_set_unique_id(
-                base_unique_id(valid[STOP_GTFS], valid[ROUTE])
+                base_unique_id(valid[STOP_GTFS], valid[ROUTE], valid[DESTINATION])
             )
             self._abort_if_unique_id_configured()
-
             if valid.get(ERROR, "") == "":
-                title = f"{valid[STOP_NAME]}({valid[STOP_CODE]}) {valid[ROUTE]}"
+
+                title = ""
+                if valid[ROUTE] is not None:
+                    title = f"{valid[STOP_NAME]}({valid[STOP_CODE]}) {valid[ROUTE]}"
+                elif valid[DESTINATION] is not None:
+                    title = f"{valid[STOP_NAME]}({valid[STOP_CODE]}) {valid[DESTINATION]}"
+                else:
+                    title = f"{valid[STOP_NAME]}({valid[STOP_CODE]}) ALL"
                 return self.async_create_entry(title=title, data=valid)
             else:
                 reason = valid.get(ERROR, "Configuration Error!")
                 _LOGGER.error(reason)
                 return self.async_abort(reason=reason)
 
-            errors[DOMAIN] = valid[ERROR]
-
         data_schema = vol.Schema(
             {
                 vol.Required(NAME_CODE, default=""): str,
-                vol.Optional(ROUTE, default="ALL"): str
+                vol.Required(ROUTE, default="ALL"): str,
+                vol.Required(DESTINATION, default="ALL"): str
             }
         )
 
